8b8444668e70d02f34c0c8490fa0bfd53c673dd6
young <kjaa3@naver.com>
2023-11-01 11:21:26 +0900

    커밋푸쉬 왜안돼

--- src/main/java/com/kosa/pro/config/CommonConfig.java
+++ src/main/java/com/kosa/pro/config/CommonConfig.java
@@ -26,7 +26,7 @@ public class CommonConfig {
 //    	tilesViewResolver.setOrder(0);
 //    	return tilesViewResolver;
 //    }
-	//////ㅇㅇㅇㅇㅇㅇㅇㅇ
+	//////ㅇㅇㅇㅇㅇㅇㅇㅇ아니 왜 안되냐고
 
 //    @Bean
 //    public TilesConfigurer tilesConfigurer() {
