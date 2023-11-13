a8902a49b5499dd56712f5c66f7cb8c10213108c
young <kjaa3@naver.com>
2023-11-14 08:57:18 +0900

    [young]11.14 - 봉사모집게시판 다시 구현

---
+++ src/main/java/com/kosa/pro/model/search/RecruitSearchVO.java
@@ -0,0 +1,13 @@
+package com.kosa.pro.model.search;
+import org.springframework.web.multipart.MultipartFile;
+import com.kosa.pro.model.common.SearchVO;
+import lombok.Data;
+
+
+@Data
+public class RecruitSearchVO extends SearchVO {
+	
+	private static final long serialVersionUID = -6342602198186321293L;
+	
+	
+}
