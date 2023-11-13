0485d84b949e1742347cfc1678e22f7dc950aeb0
민재 <minjae1163@gmail.com>
2023-11-13 10:57:25 +0900

    [민재|11-13] - 2주차 월요일 시작 커밋

--- src/main/java/com/kosa/pro/service/admin/AdminChatRoomService.java
+++ src/main/java/com/kosa/pro/service/admin/AdminChatRoomService.java
@@ -15,13 +15,44 @@ import com.kosa.pro.service.common.BaseService;
 public class AdminChatRoomService extends BaseService{
 
 	// 봉사신청 게시글에 따른 신청자들과 단체회원 채팅방 목록
-			public Map<String, List<ChatRoomVO>> findAllRecruitChatRoom(MemberSearchVO search) throws Exception {
+//			public Map<String, List<ChatRoomVO>> findAllRecruitChatRoom(MemberSearchVO search) throws Exception {
+//
+//				Map<String, List<ChatRoomVO>> chatRoomMap = new HashMap<>();
+//				
+//				chatRoomMap.put("RecruitChatRoomList", (List<ChatRoomVO>) getDAO().selectBySearch("adminchat.findAllRecruitChatRoom",
+//						search, "totalCountChatroom"));
+//				System.out.println("채팅 서비스 뭐 있음-->" + chatRoomMap);
+//				return chatRoomMap;
+//			}
+			
+			  // 봉사신청 게시글에 따른 신청자들과 단체회원 채팅방 목록
+		    public Map<String, List<ChatRoomVO>> findAllRecruitChatRoom(MemberSearchVO search) throws Exception {
 
-				Map<String, List<ChatRoomVO>> chatRoomMap = new HashMap<>();
-				
-				chatRoomMap.put("RecruitChatRoomList", (List<ChatRoomVO>) getDAO().selectBySearch("adminchat.findAllRecruitChatRoom",
-						search, "totalCountChatrecruit"));
-				System.out.println("채팅 서비스 뭐 있음-->" + chatRoomMap);
-				return chatRoomMap;
-			}
-}
+		        Map<String, List<ChatRoomVO>> chatRoomMap = new HashMap<>();
+
+		        // MyBatis 쿼리 실행 이전에 로그 디버깅 추가
+		        System.out.println("채팅 서비스 - findAllRecruitChatRoom 호출");
+		        System.out.println("파라미터 search: " + search);
+
+		        List<ChatRoomVO> chatRoomList = (List<ChatRoomVO>) getDAO().selectBySearch("adminchat.findAllRecruitChatRoom",
+		                search, "totalCountChatroom");
+
+		        // MyBatis 쿼리 결과 확인
+		        System.out.println("채팅 서비스 - 조회 결과 chatRoomList: " + chatRoomList);
+
+		        // 결과 객체를 모델에 추가하기 전에 로그 디버깅 추가
+		        chatRoomMap.put("RecruitChatRoomList", chatRoomList);
+		        System.out.println("채팅 서비스 - chatRoomMap: " + chatRoomMap);
+
+		        return chatRoomMap;
+ }
+	
+
+
+
+
+
+}		
+			
+			
+
