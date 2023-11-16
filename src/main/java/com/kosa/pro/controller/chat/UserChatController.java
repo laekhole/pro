7d3c980e65159aa7bd8e2549f24b4fb6b2c2118b
민재 <minjae1163@gmail.com>
2023-11-16 10:16:47 +0900

    [민재]-최종 - 김민재 [민재|11.15-최종]

--- src/main/java/com/kosa/pro/controller/chat/UserChatController.java
+++ src/main/java/com/kosa/pro/controller/chat/UserChatController.java
@@ -9,9 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kosa.pro.controller.admin.AdminChatController;
 import com.kosa.pro.model.ChatRoomVO;
+import com.kosa.pro.model.VolunteerProceedVO;
 import com.kosa.pro.model.search.MemberSearchVO;
 import com.kosa.pro.service.UserChatRoomService;
 
@@ -27,13 +29,23 @@ public class UserChatController {
 
 //	채팅방 단일 뷰 페이지를 반환하는 핸들러
 	@GetMapping("/chatRoom")
-	public String findmyChatRoom(@PathVariable int memSeq , Model model) throws Exception {
+	public String findmyChatRoom(@RequestParam(name = "memSeq") String memSeq, Model model) throws Exception {
 		
 		log.info("나의 채팅 목록 메인뷰");
-		ChatRoomVO mychatInfo = _userChatRoomService.findmyChatRoom(memSeq);
+		VolunteerProceedVO mychatInfo = _userChatRoomService.findmyChatRoom(memSeq);
+		model.addAttribute("mychatinfo" , mychatInfo);
+		model.addAttribute("memSeq" , memSeq);
+		System.out.println("컨트롤러 ->> " + mychatInfo);
 		
-	return "chat/myChat";
+	return "chat/myChatRoomList";
 }
+
+	// 채팅 방 입장 페이지를 반환하는 핸들러
+		@GetMapping("/room/enter")
+		public String roomEnter(Model model) {
+			return "/chat/myChatRoom";
+		}
+	
 	
 }
 
--- src/main/webapp/WEB-INF/jsp/admin/adminchat.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/adminchat.jsp
@@ -148,7 +148,6 @@
                     </div>
                 </div>
             </div>
->>>>>>> branch 'develop' of https://github.com/onlyminjae/pro.git
 
 				<div class="inbox_chat">
 					<c:forEach var="chatRoomItem" items="${chatlist}">
@@ -424,5 +423,3 @@
         }
 
 </script>
-=======
->>>>>>> branch 'develop' of https://github.com/onlyminjae/pro.git
